7264c0dd30e50c4f5a4b9da90c57889415c380f5
Matias <matiasdx@proton.me>
2025-12-28 13:08:53 -0300

    Add exercise 2-3: htoi

---
+++ Chapter-2/2-3/htoi.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+int htoi(char h[]);
+
+int main(void){
+    char chara[] = "0x1F";
+    int caracter=htoi(chara);
+    printf("%d\n",caracter);
+    return 0;
+}
+
+int htoi (char h[]){
+    int i,n;
+    i=0;
+    n=0;
+    if (h[0]=='0' && (h[1]=='x' || h[1]=='X')){
+        i=2;
+    }
+    while ((h[i]>='0' && h[i]<='9') || (tolower(h[i])>='a' && tolower(h[i])<='f')){
+        if (tolower(h[i])>='0' && tolower(h[i])<='9'){
+            n=n*16+(h[i]-'0');
+        }else{
+            h[i]=tolower(h[i]);
+            n=n*16+(h[i]-'a'+10);
+        }
+        ++i;
+    }
+    return n;
+}
